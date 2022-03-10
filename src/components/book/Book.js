436d7f27626f21198882c3e340def39d62afb3d4
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-03-10 20:38:26 +1100

    First Attempt

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -13,9 +13,10 @@ const Book = () => {
             <div className='form-container'>
                 <form action='https://getform.io/f/e30025fb-f4b3-4514-8482-623594a19a3c' method='POST'>
                     <input className='email' type="email" name='email' placeholder='Enter your email'></input>
+                    <p>By clicking submit, you agree to recieve email communication from Faded Barber llc</p>
+                    <button className='btn' type='submit'>Submit</button>
                 </form>
-                <p>By clicking submit, you agree to recieve email communication from Faded Barber llc</p>
-                <button className='btn'>Submit</button>
+
             </div>
         </div>
     )
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <h1>Faded</h1>
+            <hr></hr>
+            <p>Site Developed by Canaan de los Santos all rights reserved</p>
+        </div>
+    )
+}
+
+export default Footer
